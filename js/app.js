604b6003e70bfff0eeb729dbf203bd06c67ef859
Tommalieh <muhammedtommalieh@gmail.com>
2020-03-02 10:24:59 +0200

    Fixed Naming

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 var items = [
-    'bag',
-    'banana',
-    'bathroom',
-    'boots',
-    'breakfast',
-    'bubblegum',
-    'chair',
-    'cthulhu',
-    'dog-duck',
-    'dragon',
-    'pen',
-    'pet-sweep',
-    'scissors',
-    'shark',
-    'sweep',
-    'tauntaun',
-    'unicorn',
-    'usb',
-    'water-can',
-    'wine-glass'
+    'bag.jpg',
+    'banana.jpg',
+    'bathroom.jpg',
+    'boots.jpg',
+    'breakfast.jpg',
+    'bubblegum.jpg',
+    'chair.jpg',
+    'cthulhu.jpg',
+    'dog-duck.jpg',
+    'dragon.jpg',
+    'pen.jpg',
+    'pet-sweep.jpg',
+    'scissors.jpg',
+    'shark.jpg',
+    'sweep.png',
+    'tauntaun.jpg',
+    'unicorn.jpg',
+    'usb.gif',
+    'water-can.jpg',
+    'wine-glass.jpg'
 ]
 
 var itemsCreated = [];
@@ -33,8 +33,8 @@ var event;
 
 
 function Item(name){
-    this.name = name;
-    this.imageUrl = `img/${this.name}.jpg`;
+    this.name = name.split(',')[0];
+    this.imageUrl = `img/${this.name}`;
     this.clicks = 0;
     this.views = 0;
     itemsCreated.push(this);
@@ -67,19 +67,19 @@ function randomImage(e){
     event = e;
 
     for (var k = 0; k < itemsCreated.length; k++){
-        if (event.target.alt === itemsCreated[k].name){
+        if (event.target.alt === itemsCreated[k].name.split(',')[0]){
             itemsCreated[k].clicks = itemsCreated[k].clicks + 1;
         }
     }
 
     for (var l = 0; l < itemsCreated.length; l++){
-        if (leftImage.alt === itemsCreated[l].name){
+        if (leftImage.alt === itemsCreated[l].name.split(',')[0]){
             itemsCreated[l].views = itemsCreated[l].views + 1;
         }
-        if (centerImage.alt === itemsCreated[l].name){
+        if (centerImage.alt === itemsCreated[l].name.split(',')[0]){
             itemsCreated[l].views = itemsCreated[l].views + 1;
         }
-        if (rightImage.alt === itemsCreated[l].name){
+        if (rightImage.alt === itemsCreated[l].name.split(',')[0]){
             itemsCreated[l].views = itemsCreated[l].views + 1;
         }
     }
@@ -101,7 +101,7 @@ createThreeRandImages();
 
 
 
-    if ((leftImageAttribute === centerImageAttribute || leftImageAttribute === rightImageAttribute) || centerImageAttribute  === rightImageAttribute){
+    while ((leftImageAttribute === centerImageAttribute || leftImageAttribute === rightImageAttribute) || centerImageAttribute  === rightImageAttribute){
         console.log(event);
         createThreeRandImages();
     }
@@ -133,19 +133,19 @@ function createThreeRandImages(){
     var randomNumber = randNumb(0, itemsCreated.length -1);
     leftImage = document.getElementById('left');
     leftImage.setAttribute('src', itemsCreated[randomNumber].imageUrl);
-    leftImage.setAttribute('alt', itemsCreated[randomNumber].name);
+    leftImage.setAttribute('alt', itemsCreated[randomNumber].name.split(',')[0]);
     leftImageAttribute = leftImage.getAttribute('alt');
 
     var randomNumber = randNumb(0, itemsCreated.length -1);
     centerImage = document.getElementById('center');
     centerImage.setAttribute('src', itemsCreated[randomNumber].imageUrl);
-    centerImage.setAttribute('alt', itemsCreated[randomNumber].name);
+    centerImage.setAttribute('alt', itemsCreated[randomNumber].name.split(',')[0]);
     centerImageAttribute = centerImage.getAttribute('alt');
 
     var randomNumber = randNumb(0, itemsCreated.length -1);
     rightImage = document.getElementById('right');
     rightImage.setAttribute('src', itemsCreated[randomNumber].imageUrl);
-    rightImage.setAttribute('alt', itemsCreated[randomNumber].name);
+    rightImage.setAttribute('alt', itemsCreated[randomNumber].name.split(',')[0]);
     rightImageAttribute = rightImage.getAttribute('alt');
 
 }
@@ -159,7 +159,7 @@ function createReport(){
     for (var i = 0; i < items.length; i++){
         var listItem = document.createElement('li');
         list.appendChild(listItem);
-        listItem.textContent=`${itemsCreated[i].name} had ${itemsCreated[i].clicks} votes and was shown ${itemsCreated[i].views} times  `;
+        listItem.textContent=`${itemsCreated[i].name.split(',')[0]} had ${itemsCreated[i].clicks} votes and was shown ${itemsCreated[i].views} times  `;
         console.log(listItem);
     }
     
